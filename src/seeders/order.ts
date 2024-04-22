de2b50dd4206ba25718b3922bd66e938f7a65c05
akinsuyitaiwo <teeboidayo@gmail.com>
2024-04-22 11:04:29 +0100

    build:app build complete

--- src/seeders/order.ts
+++ src/seeders/order.ts
@@ -1,343 +1,343 @@
 export const orders = 
     [
         {
-          "id": "7f1d6d7e-ab44-47c9-94e9-30e7fc6f93b5",
-          "userId": "7f1d6d7e-ab44-47c9-30e7fc6f93b5",
+          "id": "70a29328-e1e3-4490-99ab-59a47abc8620",
+          "userId": "4dee9dfb-e1f9-491b-88e2-287e3a7c56ea",
           "title": "Order 1",
           "deliveryAdress": "1234 Example St, City, Country",
           "payment": "card"
         },
         {
-          "id": "7f1d6d7e-ab44-47c9-94e9-30e7fc6f95",
-          "userId": "ab44-47c9-94e9-30e7fc6f93b5",
+          "id": "fb100197-e668-4f51-ab82-64b01c597a52",
+          "userId": "4dee9dfb-e1f9-491b-88e2-287e3a7c56ea",
           "title": "Order 2",
           "deliveryAdress": "5678 Sample Ave, Town, Country",
           "payment": "card"
         }, 
         {
-          "id": "7f1d6d7e-ab4-47c9-94e9-30e7fc6f95",
-          "userId": "ab44-479-94e9-30e7fc6f93b5",
+          "id": "b3c907cc-ab25-4674-8e38-3b2b7d187a4c",
+          "userId": "4dee9dfb-e1f9-491b-88e2-287e3a7c56ea",
           "title": "Order 3",
           "deliveryAdress": "5678 Sample Ave, Town, Nigeria",
           "payment": "card"
         },
         {
-          "id": "4-ab4-47c9-94e9-30e7fc6f95",
-          "userId": "ab434-479-94e9-30e7fc6f93b5",
+          "id": "96f0bd5a-8623-443a-b1ab-2540ba119502",
+          "userId": "4dee9dfb-e1f9-491b-88e2-287e3a7c56ea",
           "title": "Order 5",
           "deliveryAdress": "5678 Sample Ave, Town, asia",
           "payment": "card"
         },
         {
-          "id": "ab4-47c9-94e9-30e7fc6f95",
-          "userId": "ab34-479-94e9-30e7fc6f93b5",
+          "id": "b5bbc5b2-e2b6-48fb-b71c-c7692cce5677",
+          "userId": "4dee9dfb-e1f9-491b-88e2-287e3a7c56ea",
           "title": "Order 6",
           "deliveryAdress": "5678 Sample Ave, Town, yaba",
           "payment": "card"
         },
         {
-          "id": "ab4-47c9-94e9-30e7fc6f953",
-          "userId": "ab34-479-94e9-30e7f93b58",
+          "id": "87eb3480-022c-46f5-9608-83e2ee4aba46",
+          "userId": "2e80b813-f659-456d-93f9-1887917123cb",
           "title": "Order 7",
           "deliveryAdress": "5678 Sample Ave, bariga, yaba",
           "payment": "card"
         },
         {
-          "id": "ab4-47c9-94e9-30e7fc6f9538",
-          "userId": "ab34-479-94e9-37fc6f93b58",
+          "id": "9db3597f-0d3a-4ba2-9316-5e70c984f928",
+          "userId": "2e80b813-f659-456d-93f9-1887917123cb",
           "title": "Order 8",
           "deliveryAdress": "5678, bariga, yaba",
           "payment": "card"
         },
         {
-          "id": "ab4-47c9-94e9-30e7fc6f953",
-          "userId": "ab34-479-94e9-30e7fcuh93b58",
+          "id": "a22b0ce3-ded3-43bb-b3be-e9f803c201a0",
+          "userId": "2e80b813-f659-456d-93f9-1887917123cb",
           "title": "Order 9",
           "deliveryAdress":  "Sample Ave, bariga, yaba",
           "payment": "card"
         },
         {
-          "id": "ab4855-47c9-94e9-30e7fc6f953",
-          "userId": "ab3674-479-94e9-30e7fc6f93b58",
+          "id": "a47521d7-9593-4d9a-99f3-b7052a589487",
+          "userId": "2e80b813-f659-456d-93f9-1887917123cb",
           "title": "Order 10",
           "deliveryAdress": "yaba",
           "payment": "card"
         },
         {
-          "id": "ab4-47c9-9104e9-30e7fc6f953",
-          "userId": "ab34-41179-94e9-30e7fc6f93b58",
+          "id": "a47521d7-9593-4d9a-99f3-b7052a589487",
+          "userId": "2e80b813-f659-456d-93f9-1887917123cb",
           "title": "Order 11",
           "deliveryAdress": "5678 Sample Ave, bariga",
           "payment": "card"
         },
         {
-          "id": "ab4-47c9-94e9-30e7fc6f95133",
-          "userId": "ab34-479-94e9-30e7fc6f93b5813",
+          "id": "62c341a2-4ed5-4ef7-ab64-904b9197b823",
+          "userId": "31696c84-9791-41e2-8a55-0087d00d2163",
           "title": "Order 12",
           "deliveryAdress": "unilag",
           "payment": "card"
         },
         {
-          "id": "ab4-47c9-94e9-30e7fc6f95312",
-          "userId": "ab34-479-94e9-30e7fc6f93b5812",
+          "id": "02b452df-5acc-4222-85ae-738b9dce94d6",
+          "userId": "31696c84-9791-41e2-8a55-0087d00d2163",
           "title": "Order 13",
           "deliveryAdress": "ladylak",
           "payment": "card"
         },
         {
-          "id": "ab4-47c9-94e9-30e7fc6f95314",
-          "userId": "ab34-479-94e9-30e7fc6f93b5148",
+          "id": "5042d4c9-6777-4a99-8a65-ca3199bf3316",
+          "userId": "31696c84-9791-41e2-8a55-0087d00d2163",
           "title": "Order 14",
           "deliveryAdress": "ilupeju",
           "payment": "card"
         },
         {
-          "id": "ab4-47c9-94e9-30e7fc6f95315",
-          "userId": "ab34-479-94e9-30e7fc6f93b5815",
+          "id": "0aee88a9-c554-4dd5-9c9b-200611509529",
+          "userId": "31696c84-9791-41e2-8a55-0087d00d2163",
           "title": "Order 15",
           "deliveryAdress": "biobaku",
           "payment": "card"
         },
         {
-          "id": "ab4-47c9-94e9-30e7fc6f95316",
-          "userId": "ab34-479-94e9-30e7fc6f93b5816",
+          "id": "6f97585f-e72d-448b-8a51-68d099c7cb2d",
+          "userId": "31696c84-9791-41e2-8a55-0087d00d2163",
           "title": "Order 16",
           "deliveryAdress": "amina",
           "payment": "card"
         },
         {
-          "id": "ab4-47c9-94e9-30e7fc6f95317",
-          "userId": "ab34-479-94e9-30e7fc6f93b5178",
+          "id": "14542767-4534-4044-9624-0fd799406774",
+          "userId": "31696c84-9791-41e2-8a55-0087d00d2163",
           "title": "Order 17",
           "deliveryAdress": "ojota",
           "payment": "card"
         },
         {
-          "id": "ab4-47c9-94e9-30e7fc6f95183",
-          "userId": "ab34-479-94e9-30e7fc6f93b5818",
+          "id": "b9509cb3-e8c2-4a15-9356-c97959ef44b2",
+          "userId": "913daa03-23c4-4593-af41-a8a96d10a787",
           "title": "Order 18",
           "deliveryAdress": "akoka",
           "payment": "card"
         },
         {
-          "id": "ab4-47c9-94e9-30e7fc6f95319",
-          "userId": "ab34-479-94e9-30e7fc6f93b5819",
+          "id": "65548028-4a38-43ab-b712-271f2920dc03",
+          "userId": "913daa03-23c4-4593-af41-a8a96d10a787",
           "title": "Order 19",
           "deliveryAdress": "marylanda",
           "payment": "card"
         },
         {
-          "id": "ab4-47c9-94e9-30e7fc6f95320",
-          "userId": "ab34-479-94e9-30e7fc6f93b5820",
+          "id": "6654021a-b33f-49c3-bb77-83591f1b3255",
+          "userId": "913daa03-23c4-4593-af41-a8a96d10a787",
           "title": "Order 20",
           "deliveryAdress": "bokku",
           "payment": "card"
         },
         {
-          "id": "ab4-47c9-94e9-30e7fc6f95321",
-          "userId": "ab34-479-94e9-30e7fc6f93b5218",
+          "id": "906b0ca2-ba8b-45d4-873a-67799591cc5c",
+          "userId": "913daa03-23c4-4593-af41-a8a96d10a787",
           "title": "Order 21",
           "deliveryAdress": "shoprite",
           "payment": "card"
         },
         {
-          "id": "ab4-47c9-94e9-30e7fc6f95322",
-          "userId": "ab34-479-94e9-30e7fc6f93b5228",
+          "id": "986ab49b-cbb3-4f59-ad9b-6f29b759d608",
+          "userId": "aaba7d7d-b69b-4717-beb0-b7067f05aad2",
           "title": "Order 22",
           "deliveryAdress": "ipaja",
           "payment": "card"
         },
         {
-          "id": "ab4-47c9-94e9-30e7fc6f95323",
-          "userId": "ab34-479-94e9-30e7fc6f93b5823",
+          "id": "aad919f2-9f35-44fa-b98b-ac6779d5f8a9",
+          "userId": "aaba7d7d-b69b-4717-beb0-b7067f05aad2",
           "title": "Order 23",
           "deliveryAdress": "otta",
           "payment": "card"
         },
         {
-          "id": "ab4-47c9-94e9-30e7fc6f95324",
-          "userId": "ab34-479-94e9-30e7fc6f93b5824",
+          "id": "b000c6df-81e4-4bfc-8c97-67c7a525e6a6",
+          "userId": "aaba7d7d-b69b-4717-beb0-b7067f05aad2",
           "title": "Order 24",
           "deliveryAdress": "ayobo",
           "payment": "card"
         },{
-          "id": "ab4-47c9-94e9-30e7fc6f95325",
-          "userId": "ab34-479-94e9-30e7fc6f93b5258",
+          "id": "49f103cc-0160-4166-a0d0-3e9a056ffac3",
+          "userId": "aaba7d7d-b69b-4717-beb0-b7067f05aad2",
           "title": "Order 25",
           "deliveryAdress": "5678 Sample Ave, bariga, gbagada",
           "payment": "card"
         },
         {
-          "id": "ab4-47c9-94e9-30e7fc6f95326",
-          "userId": "ab34-479-94e9-30e7fc6f93b5826",
+          "id": "773a4084-da84-4c1d-936b-8f74ce7b4eca",
+          "userId": "aaba7d7d-b69b-4717-beb0-b7067f05aad2",
           "title": "Order 26",
           "deliveryAdress": "adebo cl",
           "payment": "card"
         },
         {
-          "id": "ab4-47c9-94e9-30e7fc6f95327",
-          "userId": "ab34-479-94e9-30e7fc6f93b5827",
+          "id": "e19b5ca9-71fb-47cd-9778-b1eadde35e80",
+          "userId": "9d2c90ff-d2d6-4c18-9790-8b4d8d5cfcc4",
           "title": "Order 27",
           "deliveryAdress": "festac town",
           "payment": "cash"
         },
         {
-          "id": "ab4-47c9-94e9-30e7fc6f95328",
-          "userId": "ab34-479-94e9-30e7fc6f93b5828",
+          "id": "38e27cd0-84f2-49a7-93ed-6893cd082bd1",
+          "userId": "9d2c90ff-d2d6-4c18-9790-8b4d8d5cfcc4",
           "title": "Order 28",
           "deliveryAdress": "bayrock",
           "payment": "cash"
         },
         {
-          "id": "ab4-47c9-94e9-30e7fc6f95293",
-          "userId": "ab34-479-94e9-30e7fc6f93b5829",
+          "id": "bea12ca7-c781-4872-94a3-c7f1370919e1",
+          "userId": "9d2c90ff-d2d6-4c18-9790-8b4d8d5cfcc4",
           "title": "Order 29",
           "deliveryAdress": "adebo close",
           "payment": "cash"
         },
         {
-          "id": "ab4-47c9-94e9-30e7fc6f95330",
-          "userId": "ab34-479-94e9-30e7fc6f93b5830",
+          "id": "52666a30-391d-4bab-95c7-46a3e317a461",
+          "userId": "9d2c90ff-d2d6-4c18-9790-8b4d8d5cfcc4",
           "title": "Order 30",
           "deliveryAdress": "wilmer street",
           "payment": "cash"
         },
         {
-          "id": "ab4-47c9-94e9-30e7fc6f95331",
-          "userId": "ab34-479-94e9-30e7fc6f93b3158",
+          "id": "9bad4b51-2c8a-4c13-9319-537d917b5aa4",
+          "userId": "9d2c90ff-d2d6-4c18-9790-8b4d8d5cfcc4",
           "title": "Order 31",
           "deliveryAdress": "osun state",
           "payment": "cash"
         },
         {
-          "id": "ab4-47c9-94e9-30e7fc6f95323",
-          "userId": "ab34-479-94e9-30e7fc6f93b3258",
+          "id": "de950bc8-01f7-4102-9ea4-db235d0bca5d",
+          "userId": "df2b862b-bb29-459f-ae70-7fb19cd9a2b9",
           "title": "Order 32",
           "deliveryAdress": "ogun state",
           "payment": "cash"
         },
         {
-          "id": "ab4-47c9-94e9-30e7fc6f95333",
-          "userId": "ab34-479-94e9-30e7fc6f93b5833",
+          "id": "ad67487d-605c-4a86-b98a-f6ede95fc027",
+          "userId": "df2b862b-bb29-459f-ae70-7fb19cd9a2b9",
           "title": "Order 33",
           "deliveryAdress": "ondo state",
           "payment": "cash"
         },
         {
-          "id": "ab4-47c9-94e9-30e7fc6f95334",
-          "userId": "ab34-479-94e9-30e7fc6f93b5348",
+          "id": "0865c785-6c86-41b6-af1e-00b2c9abb5ab",
+          "userId": "df2b862b-bb29-459f-ae70-7fb19cd9a2b9",
           "title": "Order 34",
           "deliveryAdress": "Abia state",
           "payment": "cash"
         },
         {
-          "id": "ab4-47c9-94e9-30e7fc6f95335",
-          "userId": "ab34-479-94e9-30e7fc6f93b5835",
+          "id": "0865c785-6c86-41b6-af1e-00b2c9abb5ab",
+          "userId": "df2b862b-bb29-459f-ae70-7fb19cd9a2b9",
           "title": "Order 35",
           "deliveryAdress": "Anambra state",
           "payment": "cash"
         },
         {
-          "id": "ab4-47c9-94e9-30e7fc6f95336",
-          "userId": "ab34-479-94e9-30e7fc6f93b5836",
+          "id": "43e492e1-2321-4759-8905-4798e56a4711",
+          "userId": "df2b862b-bb29-459f-ae70-7fb19cd9a2b9",
           "title": "Order 36",
           "deliveryAdress": "benin city",
           "payment": "cash"
         },
         {
-          "id": "ab4-47c9-94e9-30e7fc6f95337",
-          "userId": "ab34-479-94e9-30e7fc6f93b5378",
+          "id": "7fd6c4cd-b184-4f7a-bec5-88e0ed966de7",
+          "userId": "f6a47b09-0898-4b78-a630-9ca45207d9fc",
           "title": "Order 37",
           "deliveryAdress": "lagos state",
           "payment": "cash"
         },
         {
-          "id": "ab4-47c9-94e9-30e7fc6f95338",
-          "userId": "ab34-479-94e9-30e7fc6f93b5388",
+          "id": "e4902648-b5e0-4b3e-8e6f-ba350e0b3d58",
+          "userId": "f6a47b09-0898-4b78-a630-9ca45207d9fc",
           "title": "Order 38",
           "deliveryAdress": "Ibadan state",
           "payment": "cash"
         },
         {
-          "id": "ab4-47c9-94e9-30e7fc6f954933",
-          "userId": "ab34-479-94e9-30e7fc6f93b3958",
+          "id": "c9126414-877c-4c54-9e1d-a6dce5944249",
+          "userId": "f6a47b09-0898-4b78-a630-9ca45207d9fc",
           "title": "Order 39",
           "deliveryAdress": "Ilorin state",
           "payment": "cash"
         },
         {
-          "id": "ab4-47c9-94e9-30e7fc6f95403",
-          "userId": "ab34-479-94e9-30e7fc6f93b5408",
+          "id": "0448a719-b6dd-4fe5-a1f5-d25b19bb14f7",
+          "userId": "f6a47b09-0898-4b78-a630-9ca45207d9fc",
           "title": "Order 40",
           "deliveryAdress": "Namibia",
           "payment": "cash"
         },
         {
-          "id": "ab4-47c9-94e9-30e7fc6f95341",
-          "userId": "ab34-479-94e9-30e7fc6f9341b58",
+          "id": "c4c1c154-589b-408e-87f6-aee831500663",
+          "userId": "f6a47b09-0898-4b78-a630-9ca45207d9fc",
           "title": "Order 41",
           "deliveryAdress": "Ajegunle",
           "payment": "cash"
         },
         {
-          "id": "ab4-47c9-94e9-30e7fc6f94253",
-          "userId": "ab34-479-94e9-30e742fc6f93b58",
+          "id": "6e1f7223-dce0-4be5-92d4-6dd07164f492",
+          "userId": "4bce1247-13c1-4441-8b2b-b80571473b5e",
           "title": "Order 42",
           "deliveryAdress": "Laspotecch",
           "payment": "cash"
         },
         {
-          "id": "ab4-47c9-94e9-30e7fc6f95343",
-          "userId": "ab34-479-94e9-30e7fc6f93b5438",
+          "id": "7105eedb-998a-422c-8411-6aeeb3cc510d",
+          "userId": "4bce1247-13c1-4441-8b2b-b80571473b5e",
           "title": "Order 43",
           "deliveryAdress": "yabatech",
           "payment": "cash"
         },
         {
-          "id": "ab4-47c9-44-30e7fc6f953",
-          "userId": "ab34-44-94e9-30e7fc6f93b58",
+          "id": "504d5c2c-146d-453a-aee4-dc78e02cfeda",
+          "userId": "4bce1247-13c1-4441-8b2b-b80571473b5e",
           "title": "Order 44",
           "deliveryAdress": "shomolu",
           "payment": "cash"
         },
         {
-          "id": "ab4-47c9-94e9-30e7f45c6f953",
-          "userId": "ab34-479-94e9-3045e7fc6f93b58",
+          "id": "18267f9a-a0f1-47e8-94e9-898160ada4ec",
+          "userId": "4bce1247-13c1-4441-8b2b-b80571473b5e",
           "title": "Order 45",
           "deliveryAdress": "Idi araba",
           "payment": "cash"
         },
         {
-          "id": "ab4-47c9-94e9-30e7f45c466f953",
-          "userId": "ab34-479-94e9-304465e7fc6f93b58",
+          "id": "0f4fff0b-36bc-40be-bf53-360f7b193d1c",
+          "userId": "769d35cb-8be7-484d-a4c6-4c418bd79b83",
           "title": "Order 46",
           "deliveryAdress": "Anthony, Lagos",
           "payment": "cash"
         },
         {
-          "id": "ab4-47c9-94e9-30e7f4475c6f953",
-          "userId": "ab34-479-94e9-304547e7fc6f93b58",
+          "id": "5b11c1f6-42ea-4881-8ede-214285ce0b27",
+          "userId": "769d35cb-8be7-484d-a4c6-4c418bd79b83",
           "title": "Order 47",
           "deliveryAdress": "igando, lagos",
           "payment": "cash"
         },
         {
-          "id": "ab4-47c9-9448e9-30e7f45c6f953",
-          "userId": "ab34-479-9448e9-3045e7fc6f93b58",
+          "id": "489e99a9-c86e-4958-bc9f-59ac221b89b8",
+          "userId": "769d35cb-8be7-484d-a4c6-4c418bd79b83",
           "title": "Order 48",
           "deliveryAdress": "Sheraton, lagos",
           "payment": "cash"
         },
         {
-          "id": "ab4-47c9-94e9-3049e7f45c6f953",
-          "userId": "ab34-479-94e499-3045e7fc6f93b58",
+          "id": "504c3cff-f690-4dfb-8332-e1eb87abbd93",
+          "userId": "769d35cb-8be7-484d-a4c6-4c418bd79b83",
           "title": "Order 49",
           "deliveryAdress": "Preskin hotel",
           "payment": "cash"
         },
        {
-          "id": "ab4-47c9-94e9-3049e7f5045c6f953",
-          "userId": "ab4-47c9-94e9-3045-09e7f45c6f953",
+          "id": "69725eef-2432-4ed9-9e6b-9df90d5a0388",
+          "userId": "769d35cb-8be7-484d-a4c6-4c418bd79b83",
           "title": "Order 50",
           "deliveryAdress": "9012 Test Rd, Village, Country",
           "payment": "cash"
--- src/seeders/users.ts
+++ src/seeders/users.ts
@@ -3,52 +3,52 @@ import bcrypt from "bcrypt"
 
 export const userData = [
     {
-        "id": "7f1d6d7e-ab44-47c9-30e7fc6f93b5",
+        "id": "4dee9dfb-e1f9-491b-88e2-287e3a7c56ea",
         "email": "xyz@gmail.com",
         "password": "password"
     },
     {
-        "id": "ab44-47c9-94e9-30e7fc6f93b5",
+        "id": "2e80b813-f659-456d-93f9-1887917123cb",
         "email": "taiwo@gmail.com",
         "password": "password"
     },
     {
-        "id": "ab44-479-94e9-30e7fc6f93b5",
+        "id": "31696c84-9791-41e2-8a55-0087d00d2163",
         "email": "tyler@gmail.com",
         "password": "password"
     },
     {
-        "id": "ab44-479-94e9-30e7fc6f93b5",
+        "id": "913daa03-23c4-4593-af41-a8a96d10a787",
         "email": "hydrogin@gmail.com",
         "password": "password"
     },
     {
-        "id": "ab44-479-94e9-30e7fc6f93b5",
+        "id": "aaba7d7d-b69b-4717-beb0-b7067f05aad2",
         "email": "rodney@gmail.com",
         "password": "password"
     },
     {
-        "id": "ab44-47c9-94e9-30e7fc6f93b5",
+        "id": "9d2c90ff-d2d6-4c18-9790-8b4d8d5cfcc4",
         "email": "deji@gmail.com",
         "password": "password"
     },
     {
-        "id": "",
+        "id": "df2b862b-bb29-459f-ae70-7fb19cd9a2b9",
         "email": "temidyo@gmail.com",
         "password": "password"
     },
     {
-        "id": "",
+        "id": "f6a47b09-0898-4b78-a630-9ca45207d9fc",
         "email": "akinsuyi@gmail.com",
         "password": "password"
     },
     {
-        "id": "",
+        "id": "4bce1247-13c1-4441-8b2b-b80571473b5e",
         "email": "sabeeqlee@gmail.com",
         "password": "password"
     },
     {
-        "id": "",
+        "id": "769d35cb-8be7-484d-a4c6-4c418bd79b83",
         "email": "alagba@gmail.com",
         "password": "password"
     }
